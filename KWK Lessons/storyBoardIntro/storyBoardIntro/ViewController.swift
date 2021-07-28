34199a9c08f5736f787b1b5ef32b06518fa05dbb
leenattia <87781802+leenattia@users.noreply.github.com>
2021-07-28 11:40:50 -0400

    first commit

---
+++ KWK Lessons/storyBoardIntro/storyBoardIntro/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  storyBoardIntro
+//
+//  Created by Scholar on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
