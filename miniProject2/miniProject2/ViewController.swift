c30753014f05aec90c548f4b52f3058f0f40e0fe
leenattia <87781802+leenattia@users.noreply.github.com>
2021-07-29 21:31:00 -0400

    mini project two updated

--- miniProject2/miniProject2/ViewController.swift
+++ miniProject2/miniProject2/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var answer1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    var answerResponse = "that's a pretty color"
+    
+    @IBAction func choice1Tapped(_ sender: UIButton) {
+        
+        answer1.text = answerResponse
+        
+    }
+    
 
+    
 }
 
