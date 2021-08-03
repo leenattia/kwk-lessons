acc32e5f2b7d63e58a8ab95a285cdb114bbc0989
leenattia <87781802+leenattia@users.noreply.github.com>
2021-08-03 09:44:04 -0400

    to do list iteration 0 complete

--- ToDoList/ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoList/ToDoTableViewController.swift
@@ -31,14 +31,10 @@ class ToDoTableViewController: UITableViewController {
     
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 2
+        return listOfToDo.count
     }
 
     
@@ -59,14 +55,17 @@ class ToDoTableViewController: UITableViewController {
     
 
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+        
+        if let nextAddToDoVC = segue.destination as?
+            AddToDoViewController {
+            nextAddToDoVC.previousToDoTVC = self
+        }
+}
+    
 }
